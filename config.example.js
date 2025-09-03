763787ec39ae4776e11da1f13d2b0c34f9509ac3
jkkkk100 <37616595+jkkkk100@users.noreply.github.com>
2025-09-03 14:52:34 +0800

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,10 @@
+// config.example.js
+module.exports = {
+  chatbase: {
+    scriptId: 'your_chatbase_script_id_here',
+    domain: 'www.chatbase.co'
+  },
+  auth: {
+    verificationSecret: 'your_verification_secret_here'
+  }
+};
